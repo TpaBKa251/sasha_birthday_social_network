1cc61a3bf805bd4054c51d530ae1ef4540897977
Лапшин Илья Романович <irl1@tpu.ru>
2025-09-03 21:56:43 +0700

    Привет 2

--- src/main/java/com/example/sasha_birthday_social_network/config/CorsConfig.java
+++ src/main/java/com/example/sasha_birthday_social_network/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://sasha-birthday-greetings.onrender.com", "https://sasha-birthday-greeting-add.onrender.com") // Укажите ваш фронтенд сервер
+                .allowedOrigins("https://hakaton-zeta.vercel.app") // Укажите ваш фронтенд сервер
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- src/main/java/com/example/sasha_birthday_social_network/config/Security.java
+++ src/main/java/com/example/sasha_birthday_social_network/config/Security.java
@@ -8,15 +8,33 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class Security {
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://hakaton-zeta.vercel.app")); // Укажите ваш фронтенд сервер
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true); // Важно: если true, нельзя использовать allowedOrigins("*")
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                //.cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(c -> c
                         .anyRequest().permitAll()
                 )
@@ -25,5 +43,4 @@ public class Security {
 
         return http.build();
     }
-
 }

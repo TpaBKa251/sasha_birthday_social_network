137c674e5c64f226837c711a453ae171dd672d49
Лапшин Илья Романович <irl1@tpu.ru>
2025-09-03 22:19:23 +0700

    Привет 2

--- src/main/java/com/example/sasha_birthday_social_network/config/Security.java
+++ src/main/java/com/example/sasha_birthday_social_network/config/Security.java
@@ -2,6 +2,7 @@ package com.example.sasha_birthday_social_network.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -38,6 +39,8 @@ public class Security {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(c -> c
+                        .requestMatchers(HttpMethod.GET, "sasha/birthday/social/network/get").permitAll()
+                        .requestMatchers(HttpMethod.POST, "sasha/birthday/social/network").permitAll()
                         .anyRequest().permitAll()
                 )
                 .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
